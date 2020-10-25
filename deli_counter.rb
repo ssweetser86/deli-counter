fda711b9fb7858f2d1598e913663bd3f68576b24
Stephen <stephenjsweets@gmail.com>
2020-10-25 19:39:37 -0400

    commit tree da60c90aa7189c35346eb1a0a14da259790ff6d6

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def take_a_number(queue, name)
+    queue << name
+    puts "Welcome, #{name}. You are number #{queue.length} in line."
+end
+
+def line(queue)
+    string = "The line is currently"
+    if queue.length == 0
+        string << " empty."
+    else
+       string << ":" 
+       queue.each.with_index(1) do |name, index| 
+        string << " #{index}. #{name}"
+       end
+    end
+    puts string 
+end
+
+def now_serving(queue)
+    if queue.length == 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{queue.shift}."
+    end
+end
